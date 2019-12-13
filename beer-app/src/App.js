fb2074ab99877bebc582fd2c34c911801aa957c2
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-13 12:40:30 -0500

    setting up React for the first time. Deleting unnecessary files. Add favicon beer icon.

--- beer-app/src/App.js
+++ beer-app/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>We're up and running!</h1>
     </div>
   );
 }
