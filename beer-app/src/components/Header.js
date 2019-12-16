5b976f0721222555e666f1810f15976b323beeba
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 10:00:41 -0500

    Added links to the Header

--- beer-app/src/components/Header.js
+++ beer-app/src/components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>HEADER</div>
+    <header>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/about">About</Link>
+        </nav>
+      </header>
   )
 }
 
