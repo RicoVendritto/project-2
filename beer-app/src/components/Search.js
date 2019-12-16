086a4a180dd843eeefff708abea0b6dff858997d
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 14:17:00 -0500

    Fixed issue with onchange function

--- beer-app/src/components/Search.js
+++ beer-app/src/components/Search.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const Search = props => {
-  console.log(props.props.match.params.slug);
   const slug = props.props.match.params.slug;
   return (
-    <form onSubmit={(e) => props.handleSubmit(e, slug)}>
+    <form onSubmit={e => props.handleSubmit(e, slug)}>
       <h2>SEARCH</h2>
-      {slug !== "rbg" ? <input type="text" name="search" placeholder="type here" /> : <button name="generate">GENERATE</button>}
+      {slug !== "rbg" ? (
+        <input
+          type="text"
+          name="search"
+          onChange={props.handleChange}
+          placeholder="type here"
+        />
+      ) : (
+        <button name="generate">GENERATE</button>
+      )}
     </form>
   );
 };
