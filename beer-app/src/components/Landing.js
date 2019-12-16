2612d1be75d744e4ed1f65fffaff20867e169ecf
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 12:12:15 -0500

    Cleaned up Code

--- beer-app/src/components/Landing.js
+++ beer-app/src/components/Landing.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Landing = (props) => {
+const Landing = props => {
   return (
     <div>
       <h1>Landing Page</h1>
-      <button onClick={(e) => props.ageValidation(e, false)}>I'm younger than 21</button>
-      <button onClick={(e) => props.ageValidation(e, true)}>I'm 21 or older</button>
-      </div>
-  )
-}
+      <button onClick={e => props.ageValidation(e, false)}>
+        I'm younger than 21
+      </button>
+      <button onClick={e => props.ageValidation(e, true)}>
+        I'm 21 or older
+      </button>
+    </div>
+  );
+};
 
 export default Landing;
