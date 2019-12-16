0bd0138ed293455aa74c1d91bbacba37a0ffa2dc
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 10:33:51 -0500

    added all required API calls and test them with console.log

--- beer-app/src/components/Main.js
+++ beer-app/src/components/Main.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import SearchOptions from "./SearchOptions";
 
 //Api component
-import { beerApi, breweryApi, beerName, abvApi, ibuApi } from "../services/ApiCalls";
+import { beerApi, breweryApi, beerName, abvApi, ibuApi, ebcApi, randomBeer, foodBeerApi, breweryCity, breweryName } from "../services/ApiCalls";
 
 class Main extends Component {
   constructor(props) {
@@ -18,11 +18,21 @@ class Main extends Component {
     const result3 = await beerName("bud");
     const result4 = await abvApi(2);
     const result5 = await ibuApi(2);
+    const result6 = await ebcApi(500);
+    const result7 = await randomBeer();
+    const result8 = await foodBeerApi("pork");
+    const result9 = await breweryCity("new_york");
+    const result0 = await breweryName("empire");
     console.log(result1);
     console.log(result2);
     console.log(result3);
     console.log(result4);
     console.log(result5);
+    console.log(result6);
+    console.log(result7);
+    console.log(result8);
+    console.log(result9);
+    console.log(result0);
   };
 
   render() {
