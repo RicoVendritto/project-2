a53d787c33f9a143162ef77b18ba36fb06c381d5
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 10:02:59 -0500

    Added div elements that provide search options main.js page

---
+++ beer-app/src/components/SearchOptions.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SearchOptions = () => {
+  return (
+    <div className="searchOptions">
+      <div>Beer by Name</div>
+      <div>Alcohol by Volume</div>
+      <div>International Bitterness Volume</div>
+      <div>European Beer Convention Colour</div>
+      <div>Random Beer Generator</div>
+      <div>Beer and Food</div>
+      <div>Brewery by Name</div>
+      <div>Brewery by State</div>
+    </div>
+  );
+};
+
+export default SearchOptions;
