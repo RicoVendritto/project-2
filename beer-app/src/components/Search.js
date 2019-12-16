e85670c7b2c73fa6d1ac61db695277d4e178abab
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 16:12:36 -0500

    Made updates in Search to pass down correct information in props

--- beer-app/src/components/Search.js
+++ beer-app/src/components/Search.js
@@ -1,21 +1,27 @@
 import React from "react";
 
+//Custom component
+import Results from "./Results";
+
 const Search = props => {
   const slug = props.props.match.params.slug;
   return (
-    <form onSubmit={e => props.handleSubmit(e, slug)}>
-      <h2>SEARCH</h2>
-      {slug !== "rbg" ? (
-        <input
-          type="text"
-          name="search"
-          onChange={props.handleChange}
-          placeholder="type here"
-        />
-      ) : (
-        <button name="generate">GENERATE</button>
-      )}
-    </form>
+    <div className="search">
+      <form onSubmit={e => props.handleSubmit(e, slug)}>
+        <h2>SEARCH</h2>
+        {slug !== "rbg" ? (
+          <input
+            type="text"
+            name="search"
+            onChange={props.handleChange}
+            placeholder="type here"
+          />
+        ) : (
+          <button name="generate">GENERATE</button>
+        )}
+      </form>
+      {props.apiDataLoaded && <Results results={props.results} slug={slug} />}
+    </div>
   );
 };
 
