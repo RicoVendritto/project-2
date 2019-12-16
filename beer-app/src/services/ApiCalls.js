6a8fd157f0653c73407f4b07c48bc8536e10c476
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 10:34:11 -0500

    added all the required API calls required for the search options

--- beer-app/src/services/ApiCalls.js
+++ beer-app/src/services/ApiCalls.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
-import { geolocated } from "react-geolocated";
 
 const apiUrl = "https://api.punkapi.com/v2/beers";
 const beerSearch = "?beer_name=";
 const abvSearch = "?abv_gt=";
-const ibuSearch = "?ibu_gt";
-const randomUrl = "https://api.punkapi.com/v2/beers/random";
+const ibuSearch = "?ibu_gt=";
+const ebcSearch = "?ebc_gt=";
+const foodSearch = "?food=";
+const randomSearch = "https://api.punkapi.com/v2/beers/random";
 const apiBrewery = "https://api.openbrewerydb.org/breweries";
+const nameBrewery = "?by_name=";
+const cityBrewery = "?by_city="
 
 export async function beerApi(props) {
   const results = await axios(apiUrl);
@@ -28,12 +31,32 @@ export async function ibuApi(value, scale) {
   return results;
 }
 
+export async function ebcApi(value, scale) {
+  const results = await axios(`${apiUrl}${ebcSearch}${value}`);
+  return results;
+}
+
 export async function randomBeer(props) {
-  const results = await axios(randomUrl);
+  const results = await axios(randomSearch);
+  return results;
+}
+
+export async function foodBeerApi(food) {
+  const results = await axios(`${apiUrl}${foodSearch}${food}`);
   return results;
 }
 
 export async function breweryApi(props) {
   const results = await axios(apiBrewery);
+  return results;
+}
+
+export async function breweryName(name) {
+  const results = await axios(`${apiBrewery}${nameBrewery}${name}`);
+  return results;
+}
+
+export async function breweryCity(city) {
+  const results = await axios(`${apiBrewery}${cityBrewery}${city}`);
   return results;
 }
