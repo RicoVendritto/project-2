44bb278f16933919eb1032848e19dcb53726d860
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 12:17:29 -0500

    Added all Slugs to pass on to the Search Component. Tested. All Works. Next step, Search button

--- beer-app/src/components/SearchOptions.js
+++ beer-app/src/components/SearchOptions.js
@@ -14,31 +14,31 @@ const SearchOptions = () => {
       </div>
       <div className="searchOptions">
         <h2>Alcohol by Volume</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/abv">SEARCH</Link>
       </div>
       <div className="searchOptions">
         <h2>International Bitterness Volume</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/ibv">SEARCH</Link>
       </div>
       <div className="searchOptions">
         <h2>European Beer Convention Colour</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/ebc">SEARCH</Link>
       </div>
       <div className="searchOptions">
         <h2>Random Beer Generator</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/rbg">SEARCH</Link>
       </div>
       <div className="searchOptions">
         <h2>Beer and Food</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/baf">SEARCH</Link>
       </div>
       <div className="searchOptions">
         <h2>Brewery by Name</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/brn">SEARCH</Link>
       </div>
       <div className="searchOptions">
         <h2>Brewery by City</h2>
-        <Link to="/search">SEARCH</Link>
+        <Link to="/search/brc">SEARCH</Link>
       </div>
     </div>
   );
