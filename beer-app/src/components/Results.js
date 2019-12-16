a3b6c725181dd0400a82d1421a2c84be436bda95
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 13:38:05 -0500

    Test result component to render results on main page

--- beer-app/src/components/Results.js
+++ beer-app/src/components/Results.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Results = () => {
-  results(<div>RESULTS</div>);
-};
+const Results = props => {
+  console.log(props.results.data);
+  const results = props.results.data;
+  console.log(results);
+  return results.map((result, index) => (
+    <div key={index}>
+      <h2>{result.name}</h2>
+      <h4>{result.tagline}</h4>
+      <p>{result.description}</p>
+      <img src={result.image_url} alt="Beer Image" />
+    </div>
+  ));
+}
 
 export default Results;
