547874ff3064de41c2229c2bf09efe2fc5ed1559
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 12:13:29 -0500

    Added first slug to Link to test

--- beer-app/src/components/SearchOptions.js
+++ beer-app/src/components/SearchOptions.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+{
+  /* <Link to={"Profile/"+hero.hero_id} slug={hero.id}>Profile Info</Link> */
+}
+
 const SearchOptions = () => {
   return (
-    <div className="searchOptions">
-      <div>Beer by Name</div>
-      <div>Alcohol by Volume</div>
-      <div>International Bitterness Volume</div>
-      <div>European Beer Convention Colour</div>
-      <div>Random Beer Generator</div>
-      <div>Beer and Food</div>
-      <div>Brewery by Name</div>
-      <div>Brewery by State</div>
+    <div className="searchContainer">
+      <div className="searchOptions">
+        <h2>Beer by Name</h2>
+        <Link to="/search/bbn">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>Alcohol by Volume</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>International Bitterness Volume</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>European Beer Convention Colour</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>Random Beer Generator</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>Beer and Food</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>Brewery by Name</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
+      <div className="searchOptions">
+        <h2>Brewery by City</h2>
+        <Link to="/search">SEARCH</Link>
+      </div>
     </div>
   );
 };
