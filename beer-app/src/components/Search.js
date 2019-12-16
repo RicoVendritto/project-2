db41ae1b5cf4a22fa01d5188a598e0f14ccfad30
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 13:38:33 -0500

    Removed some absolete code

--- beer-app/src/components/Search.js
+++ beer-app/src/components/Search.js
@@ -6,7 +6,7 @@ const Search = props => {
   return (
     <form onSubmit={(e) => props.handleSubmit(e, slug)}>
       <h2>SEARCH</h2>
-      <input type="text" name="search" placeholder="type here" />
+      {slug !== "rbg" ? <input type="text" name="search" placeholder="type here" /> : <button name="generate">GENERATE</button>}
     </form>
   );
 };
