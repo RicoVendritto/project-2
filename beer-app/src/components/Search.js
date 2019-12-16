28c226adb8756cded6afbc34588a65cb89155df6
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-16 13:13:59 -0500

    Added slugs for every search operator + added form + handleSubmit function that passes on the relevant slug

--- beer-app/src/components/Search.js
+++ beer-app/src/components/Search.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const Search = props => {
   console.log(props.props.match.params.slug);
-  return <div>SEARCH</div>;
+  const slug = props.props.match.params.slug;
+  return (
+    <form onSubmit={(e) => props.handleSubmit(e, slug)}>
+      <h2>SEARCH</h2>
+      <input type="text" name="search" placeholder="type here" />
+    </form>
+  );
 };
 
 export default Search;
