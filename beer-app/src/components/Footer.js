d729c6cc02df1fad8d7f6fdbf01f2e179fa56085
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-17 09:11:49 -0500

    Adjusted Footer to make weather info available

--- beer-app/src/components/Footer.js
+++ beer-app/src/components/Footer.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
 //Custom components
 import Location from "./Location";
+import Locator from "./Test";
+import Weather from "./Weather";
 
-const Footer = () => {
-  return (
-    <footer>
-      <div>FOOTER</div>
-      <Location />
-    </footer>
-  );
-};
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherInfo: "",
+      weatherInfoLoaded: false
+    };
+  }
+
+  render() {
+    return (
+      <footer>
+        <div>FOOTER</div>
+        <Locator />
+        {/* {this.state.weatherInfoLoaded && <Weather weatherInfo={this.state.weatherInfo} />} */}
+      </footer>
+    );
+  }
+}
 
 export default Footer;
