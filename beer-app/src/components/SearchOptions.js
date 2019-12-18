0ff2801e140e6f47b58fd7601ae5e625c9472108
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2019-12-18 10:21:23 -0500

    Added onClick function to <Link /> elements that allows reset function to run in the main.js

--- beer-app/src/components/SearchOptions.js
+++ beer-app/src/components/SearchOptions.js
@@ -1,46 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SearchOptions = () => {
+const SearchOptions = (props) => {
   return (
     <div className="searchContainer">
       <div className="searchOptions">
-        <Link to="/search/bbn">
+        <Link to="/search/bbn" onClick={(e) => props.reset()}>
           <h2>Beer by Name</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/abv">
+        <Link to="/search/abv" onClick={(e) => props.reset()}>
           <h2>Alcohol by Volume</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/ibv">
+        <Link to="/search/ibv" onClick={(e) => props.reset()}>
           <h2>International Bitterness Volume</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/ebc">
+        <Link to="/search/ebc" onClick={(e) => props.reset()}>
           <h2>European Beer Convention Colour</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/rbg">
+        <Link to="/search/rbg" onClick={(e) => props.reset()}>
           <h2>Random Beer Generator</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/baf">
+        <Link to="/search/baf" onClick={(e) => props.reset()}>
           <h2>Beer and Food</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/brn">
+        <Link to="/search/brn" onClick={(e) => props.reset()}>
           <h2>Brewery by Name</h2>
         </Link>
       </div>
       <div className="searchOptions">
-        <Link to="/search/brc">
+        <Link to="/search/brc" onClick={(e) => props.reset()}>
           <h2>Brewery by City</h2>
         </Link>
       </div>
